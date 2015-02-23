cf4db1ace791ef187ea9919e9ffc89ca711fdb80
tomasz.pawlak <tompwk@gmail.com>
2015-02-23 20:58:35 +0000

    FileSource changes
    Character set and character decoder added

--- src/main/java/org/tpwk/source/FileSource.java
+++ src/main/java/org/tpwk/source/FileSource.java
@@ -2,27 +2,50 @@ package org.tpwk.source;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.stream.Stream;
 
 public class FileSource implements StringSource
 {
-    private final Path input;
+    private final Path path;
+    private final Charset charset;
+    private final CharsetDecoder decoder;
 
     private BufferedReader reader;
 
     private IOException lastException;
 
-    public FileSource(Path input)
+    public FileSource(final Path path)
     {
-        this.input = input;
+        this(path, StandardCharsets.UTF_8);
+    }
+
+    public FileSource(final Path path, final Charset charset)
+    {
+        this.path = path;
+        this.charset = charset;
+        this.decoder = null;
+    }
+
+    public FileSource(final Path path, final CharsetDecoder decoder)
+    {
+        this.path = path;
+        this.charset = null;
+        this.decoder = decoder;
     }
 
     @Override
     public Stream<String> strings() throws IOException
     {
-        reader = Files.newBufferedReader(input);
+        reader = (charset == null)
+                ? new BufferedReader(new InputStreamReader(Files.newInputStream(path), decoder))
+                : Files.newBufferedReader(path, charset);
+
         return reader.lines();
     }
 
